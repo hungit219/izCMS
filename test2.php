7fd6133f75d956cf9c6af6f59edeb4c0f39b4faf
Kevin Nguyen <hungit219@gmail.com>
2015-09-04 12:48:00 +0700

    Tao ra file test php thu 2

---
+++ test2.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    echo "<p><h1>Day la noi dung file test2.php</h1></p>";
+
+?>
+
+</body>
+</html>
