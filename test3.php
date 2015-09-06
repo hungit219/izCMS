2c80131bc54bf839545412dd439db7e75fc8b7c9
Kevin Nguyen <hungit219@gmail.com>
2015-09-06 13:05:18 +0700

    Init file test 3

---
+++ test3.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    echo "Day la noi dung file test 3";
+?>
+</body>
+</html>
